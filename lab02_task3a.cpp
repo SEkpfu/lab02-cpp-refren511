ec56bbdb4a74ed3a3c436f40c79d9bdbd36fb54b
ArseniyIvanov <arsen01112007@gmail.com>
2025-09-16 20:53:45 +0300

    Update lab02_task3a.cpp

--- lab02_task3a.cpp
+++ lab02_task3a.cpp
@@ -8,11 +8,12 @@ int main() {
 	if (x <= -2) {
 		cout << 0;
 	}
-	else if (-2 < x <= 10) {
+	else if (-2 < x && x <= 10) {
 		cout << (pow(x, 2) + 4 * x + 5);
 	}
 	else {
 		cout << (1 / (pow(x, 2) + 4 * x - 5));
 	}
 	return 0;
+
 }
