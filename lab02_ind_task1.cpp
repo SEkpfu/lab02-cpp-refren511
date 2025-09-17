adb90e516b2927ac61cdc6808578fa9429ea5752
ArseniyIvanov <arsen01112007@gmail.com>
2025-09-17 13:31:23 +0300

    Update lab02_ind_task1.cpp

--- lab02_ind_task1.cpp
+++ lab02_ind_task1.cpp
@@ -1,9 +1,9 @@
-﻿#include <iostream>
+#include <iostream>
 #include <stdlib.h>
 using namespace std;
 int main() {
 	setlocale(0, "");
-	char n; double a, b;
+	int n; double a, b;
 	cout << "Введите номер действия(целое число в диапазоне 1-4) = "; cin >> n;
 	cout << "Введите два вещественных числа A и B (B не должно равняться 0)" << endl;
 	cout << "A = "; cin >> a;
@@ -14,10 +14,10 @@ int main() {
 	}
 	else {
 		switch (n) {
-		case '1': cout << "A + B = " << a + b; break;
-		case '2': cout << "A - B = " << a - b; break;
-		case '3': cout << "A * B = " << a * b; break;
-		case '4': cout << "A / B = " << a / b; break;
+		case 1: cout << "A + B = " << a + b; break;
+		case 2: cout << "A - B = " << a - b; break;
+		case 3: cout << "A * B = " << a * b; break;
+		case 4: cout << "A / B = " << a / b; break;
 		}
 	}
 	return 0;
